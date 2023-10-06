5346ad60fb0b2cac8f4876afe511250f7ecfd5ac
mackie <macklinu@users.noreply.github.com>
2023-10-06 17:05:53 -0400

    Prepare deployment to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 })
